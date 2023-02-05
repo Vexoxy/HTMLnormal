8793046ecc275e47b88797059f7166652ed46d23
Vexoxy <117086102+Vexoxy@users.noreply.github.com>
2023-02-05 10:27:52 +0300

    Create est.js

---
+++ est.js
@@ -0,0 +1,14 @@
+let a = prompt ('Ващ возраст: ');
+
+if (a >0 &&  a<3){
+   document.write(document.img1) ;
+}
+else if (a >3 && a <10){
+    console.log('бэйби плам');
+}
+else if (a>10 && a <20){
+    console.log('Старше бэйби плама')
+}
+else{
+    console.log('старик плам')
+}
